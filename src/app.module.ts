ba2c79fb419384bc20958cb6708f91866b9f3ca9
Anton Nakonechny <anakonechny@alfabank.ru>
2024-10-19 15:23:59 +0200

    feat(*): add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { HealthMonitorModule } from './health-monitor/health-monitor.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), HealthMonitorModule],
+  imports: [ConfigModule.forRoot(), HealthMonitorModule, DatabaseModule],
 })
 export class AppModule {}
